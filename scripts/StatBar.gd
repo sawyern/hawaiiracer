4a81216b63bb67075f1aa518765ab516c599c075
Sawyer Novak <sawyern@me.com>
2023-02-08 11:51:51 -1000

    add stat bar values

--- scripts/StatBar.gd
+++ scripts/StatBar.gd
@@ -11,6 +11,9 @@ func set_name(var name : String):
 func set_bar_width(var size : float):
 	$StatBar.rect_size.x = size
 
+func set_val(var val : float):
+	$StatVal.text = val as String
+
 func update_stats(stats):
 	if stats == null:
 		set_bar_width(0)
@@ -18,3 +21,4 @@ func update_stats(stats):
 	
 	print(stats[index])
 	set_bar_width(stats[index] * 20)
+	set_val(stepify(stats[index], 0.1) * 10)
